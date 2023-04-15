cf1638935b7d9ca7987a8e147656ff781c9e1788
ch3slin <iamcheslin@gmail.com>
2023-04-15 02:24:02 +0200

    Added video after clicking a bunch of times

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 const localCount = parseInt(localStorage.getItem("count")) || 0
+const videoId = "dQw4w9WgXcQ"
 
 function App() {
   const [count, setCount] = useState(localCount)
 
   useEffect(()=>{
     localStorage.setItem("count", String(count))
+    if(count == Math.floor(Math.random() * 10 + 1)){
+      const url = `https://www.youtube.com/embed/${videoId}?autoplay=1&fs=1&modestbranding=1&playsinline=1`
+      const newWindow = window.open(url, "_blank", "fullscreen=yes")
+      newWindow.focus()
+    }
   },[count])
 
   return (
